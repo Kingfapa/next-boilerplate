d05d119e4056f2d69cba0b67eea91bdef46fb0d9
Adam Maritz <14345584+Kingfapa@users.noreply.github.com>
2021-04-22 15:27:14 +0200

    snippets v1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
-export default function IndexPage() {
-  return <div className="flex">Hello World</div>;
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
+
+interface IndexPageProps {
+    hello: 'string'
 }
+
+export const getServerSideProps: GetServerSideProps<IndexPageProps> = async () => {
+    return {
+        props: {
+            hello: 'string',
+        },
+    }
+}
+
+const IndexPage: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = () => {
+    return <div></div>
+}
+
+export default IndexPage
